fade323f1d41a4e355f050b622e0de433254bc58
RamNarayan27 <ramnarayanbahadur@gmail.com>
2021-08-27 17:42:24 +0530

    Model Integrated. Added Camera Functionality.

--- app/src/main/java/com/example/megha/Clouds.kt
+++ app/src/main/java/com/example/megha/Clouds.kt
@@ -2,10 +2,12 @@ package com.example.megha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class Clouds : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_clouds)
         // TODO : Normal linear list with the info of the different types of clouds and their info in a simple card
     }
